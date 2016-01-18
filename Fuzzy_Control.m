916ffb1ebabf86e48a4ca97fe91ed0aba8e458ec
WENNY <wenny.h@live.com>
2016-01-18 22:10:44 +1100

    cleaned up for clarity

--- Fuzzy_Control.m
+++ Fuzzy_Control.m
@@ -1,4 +1,8 @@
-function Lab_10()
+% Moving target and chaser car simulation.
+% For demonstrating the use of fuzzy control in steering.
+% Wenny Hidayat
+
+function Fuzzy_Control()
 close all
 clear all
 
@@ -7,7 +11,7 @@ figure(1) ; clf ;  hold on ;
 handleCar.shape = plot(0,0,'b');
 handleCar.centroid = plot(0,0,'c');
 handleCar.target = plot(0,0,'or');
-xlabel('x (m'); ylabel('y (m)'); axis([-20, 20, -20, 20]) ; zoom on ; hold off;
+xlabel('x (m)'); ylabel('y (m)'); axis([-20, 20, -20, 20]) ; zoom on ; hold off;
 title('Global Frame');
 
 % Defining car shape
@@ -18,7 +22,7 @@ Car.center = [mean(Car.shape(1:5,1)) mean(Car.shape(1:5,2))];
 Car.centerDefault = Car.center;
 Car.heading = atan2(Car.shape(2,2)-Car.center(2), Car.shape(2,1)-Car.center(1)); %in rad
 
-% Defining target
+% Defining target x y
 target = [10 10];
 
 % Loop (simulation)
@@ -51,6 +55,7 @@ for i=1:iterations
     trackCar(i,:) = Car.center;
     
     % Rotate THEN translate
+    % From local frame to global x y.
     local_to_global_Map_X = cos(Car.heading-pi/2)*Car.shapeDefault(:,1) - sin(Car.heading-pi/2)*Car.shapeDefault(:,2) + Car.center(1) ;
     local_to_global_Map_Y = sin(Car.heading-pi/2)*Car.shapeDefault(:,1) + cos(Car.heading-pi/2)*Car.shapeDefault(:,2) + Car.center(2);
     
@@ -61,21 +66,22 @@ for i=1:iterations
     set(handleCar.shape,'xdata',local_to_global_Map_X,'ydata',local_to_global_Map_Y );
     set(handleCar.target,'xdata',target(1),'ydata',target(2) );
     pause(0.1) ;
-    if target(1)<=10 || target(2) <=10,
-        target(1) = target(1)+2*rand(1);
-        target(2) = target(2)+rand(1);
+    % Defining the target movement
+    % Can be any
+    if target(1)<=20 && target(1)>=-20 && target(2) <=20 && target(2)>=-20,
+        target(1) = target(1)+3*(rand(1)-rand(1))
+        target(2) = target(2)+3*(rand(1)-rand(1))
     else
+        % To reset back into boundary
         target = [-5 -5];
     end;
 end;
 
 
-
+% Function for updating the car, changing the location of each points
 function [xnext, ynext, tnext] = moveModel(x,y,theta,dt,velocity,ang_velocity)
     xnext = x + velocity*dt*cos(theta);
     ynext = y + velocity*dt*sin(theta);
     tnext= theta + ang_velocity*dt;
-return;
-
 return;
 
